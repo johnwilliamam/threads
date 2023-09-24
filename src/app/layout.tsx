905e408c2d570244a1d47147931d7ebc6d706402
John William de Araujo Mesquita <j.williamd.a.m@outlook.com>
2023-09-23 22:48:47 -0300

    feat: add icon threads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-Br" className='bg-threads-bg-default text-threads-text'>
+      <head>
+        <link data-default-icon="https://static.cdninstagram.com/rsrc.php/v3/yV/r/_8T3PbCSTRI.png" rel="icon" sizes="192x192" href="https://static.cdninstagram.com/rsrc.php/v3/yV/r/_8T3PbCSTRI.png" />
+        </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
