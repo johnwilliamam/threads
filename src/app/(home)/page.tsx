f078bbf3a87009175408bb86d30720751dbffd78
John William de Araujo Mesquita <j.williamd.a.m@outlook.com>
2023-09-23 22:49:08 -0300

    feat: separate posts content

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,8 @@
-import LogoThreads from "@/app/assets/logo-threads.svg";
+import LogoThreads from "@/assets/logo-threads.svg";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { posts } from '@/utils/posts';
+import formatDistanceStrict from 'date-fns/formatDistanceStrict';
 import { Heart, MessageCircle, MoreHorizontal, Repeat2, Send } from 'lucide-react';
 import Image from "next/image";
 import DropdownMenuHome from "./dropdown";
@@ -22,53 +24,60 @@ export default function Home() {
         <section>
           <HomePost />
         </section>
+        <span className="border-b-zinc-700 h-full border-b-[1px] block ml-5 my-2"></span>
         <hr className="my-4 h-[0.5px] opacity-20" />
-        <section className={styles.container}>
-          <div className={styles.avatar}>
-            <PostAvatar src="https://github.com/johnwilliamam.png" fallbackInitials="JW" />
-          </div>
-          <div className={styles.username}>
-            <p>johnwilli_am</p>
-          </div>
-          <div className={cn(styles.more, "flex items-center gap-1")}>
-            <span>
-              00h
-            </span>
-            <Button variant="outline" size='icon' className="rounded-full border-none">
-              <MoreHorizontal />
-            </Button>
-          </div>
-          <div className={styles.separator}>
-            <span className="border-l-zinc-800 h-full border-l-[2px] block ml-5 my-5"></span>
-          </div>
-          <div className={styles.post}>
-            <p>
-              Olha só que incrivel esse clone da última rede social, promissora diga-se de passagem, do gênio Mark Zuckerberg!
-              Você já usa o Threads ou ainda prefere a X?
-              &#128516;
-            </p>
-          </div>
-          <div className={cn(styles.reply_avatar, 'm-auto')}>
-            <RepliesAvatar data={[{
-              src: "https://github.com/", fallbackInitials: "JW",
-            },
-            {
-              src: "https://github.com/johnwilliamam.png", fallbackInitials: "JW"
-            }
-            ]} />
-          </div>
-          <div className={cn(styles.actions, "flex space-x-2 mt-3")}>
-            <Heart />
-            <MessageCircle />
-            <Repeat2 />
-            <Send />
-          </div>
-          <div className={styles.likes_replies}>
-            <span className="text-sm text-neutral-500">
-              1 resposta - 99 curtidas
-            </span>
-          </div>
-        </section>
+
+        <div className="space-y-6">
+          {posts.map((post) => (
+              <><section key={post.id} className={styles.container}>
+              <div className={styles.avatar}>
+                <PostAvatar src={post.user.profilePicture.src} fallbackInitials={post.user.profilePicture.initials} />
+              </div>
+              <div className={styles.username}>
+                <p>{post.user.name}</p>
+              </div>
+              <div className={cn(styles.more, "flex items-center gap-1")}>
+                <span className="min-w-[60px] text-gray-500"
+                  title={new Date(post.createdAt).toDateString()}>
+                  {formatDistanceStrict(new Date(post.createdAt), new Date(), {
+                    addSuffix: false
+                  })}
+                </span>
+                <Button variant="outline" size='icon' className="rounded-full border-none">
+                  <MoreHorizontal />
+                </Button>
+              </div>
+              <div className={styles.separator}>
+                <span className="border-l-zinc-700 h-full border-l-[2px] block ml-5 my-2"></span>
+              </div>
+              <div className={styles.post}>
+                <p> {post.content} </p>
+              </div>
+              <div className={cn(styles.reply_avatar, 'm-auto')}>
+                <RepliesAvatar data={[{
+                  src: "https://github.com/", fallbackInitials: "JW",
+                },
+                {
+                  src: "https://github.com/johnwilliamam.png", fallbackInitials: "JW"
+                }
+                ]} />
+              </div>
+              <div className={cn(styles.actions, "flex space-x-2 mt-3")}>
+                <Heart />
+                <MessageCircle />
+                <Repeat2 />
+                <Send />
+              </div>
+              <div className={styles.likes_replies}>
+                <span className="text-sm text-neutral-500">
+                  1 resposta - 99 curtidas
+                </span>
+              </div>
+            </section>
+            <span className="border-b-zinc-700 h-full border-b-[1px] block ml-5 my-2"></span>
+            </>
+          ))}
+        </div>
       </article>
     </main>
   )
