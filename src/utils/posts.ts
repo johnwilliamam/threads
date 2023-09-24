e5e7c08f55ddfdd9181dd1618dfa6447bf6b17e3
John William de Araujo Mesquita <j.williamd.a.m@outlook.com>
2023-09-23 22:50:04 -0300

    feat: add content posts

---
+++ src/utils/posts.ts
@@ -0,0 +1,43 @@
+export const posts = [{
+    id: 1,
+    user: {
+        profilePicture: {
+            src: 'https://github.com/johnwilliamam.png',
+            initials: 'JW',
+        },
+        name: 'johnwilliamam',
+    },
+        createdAt: '2023-09-23T20:33:00',
+        content: `Olha só que incrivel esse clone da última rede social, promissora diga-se de passagem, do gênio Mark Zuckerberg!
+    Você já usa o Threads ou ainda prefere a X? 
+    &#128516`,
+        replies: {
+            count: 1,
+            avatars: [{
+                src: 'https://github.com/johnwilliamam.png',
+                initials: 'JW',
+            }]
+        },
+        likes: 99,
+},
+{
+    id: 2,
+    user: {
+        profilePicture: {
+            src: 'https://github.com',
+            initials: 'JW',
+        },
+        name: 'johnwilli_am',
+    },
+    createdAt: '2023-09-23T20:33:00.00Z',
+    content: `Que interessante não?`,
+    replies: {
+        count: 99,
+        avatars: [{
+            src: 'https://github.com/johnwilliamam.png',
+            initials: 'JW',
+        }]
+    },
+    likes: 1,
+}
+]
