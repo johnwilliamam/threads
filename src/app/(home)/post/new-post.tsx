fbbaed44f34c1d26393e966831f9c07422e35f19
John William <j,williamd.a.m@outlook.com>
2023-10-02 09:02:11 -0300

    feat: change post view

--- src/app/(home)/post/new-post.tsx
+++ src/app/(home)/post/new-post.tsx
@@ -1,41 +1,37 @@
-import { Button } from "@/components/ui/button"
 import {
   Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
+  DialogTrigger
 } from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-
+import styles from '../styles.module.css'
 export function NewPost() {
   return (
     <Dialog>
       <DialogTrigger asChild>
         <p className="text-neutral-500 flex-1 font-light">Inicie uma thread...</p>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
+      <div role="dialog" className="" tabIndex={-1}>
+        <div>
+          <div></div>
+          <div>
+            <div>
+              <span>Nova thread</span>
+            </div>
+            <div className="w-full">
+              <div className={styles.container2}>
+                <div className={styles.inputArea}><input type="text"></input></div>
+                <div className={styles.textResp}><span>Qualquer pessoa pode responder</span></div>
+                <div className={styles.buttonShare}>
+                  <div className="gap-2 w-max h-9">
+                    <div className="rounded-xl aria-disabled text-neutral border border-gray-500 hover:cursor-not-allowed" role="button" tabIndex={-1}>
+                      <div className="font-semibold text-sm m-1 text-neutral-500">Publicar</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
-      </DialogContent>
+      </div>
     </Dialog>
   )
 }
