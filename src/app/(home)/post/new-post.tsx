dd8253f359a48d3c4da28b59b2416282dfeeb256
John William de Araujo Mesquita <j.williamd.a.m@outlook.com>
2023-10-19 22:25:37 -0300

    feat: change content dialog post

--- src/app/(home)/post/new-post.tsx
+++ src/app/(home)/post/new-post.tsx
@@ -1,7 +1,9 @@
+"use client"
 import {
   Dialog,
   DialogTrigger
 } from "@/components/ui/dialog"
+import { DialogContent } from "@radix-ui/react-dialog"
 import styles from '../styles.module.css'
 export function NewPost() {
   return (
@@ -9,16 +11,15 @@ export function NewPost() {
       <DialogTrigger asChild>
         <p className="text-neutral-500 flex-1 font-light">Inicie uma thread...</p>
       </DialogTrigger>
-      <div role="dialog" className="" tabIndex={-1}>
-        <div>
-          <div></div>
-          <div>
-            <div>
+      <DialogContent>
+      <div role="dialog" className="gap-2" tabIndex={-1}>
+            <div className="gap-2">
               <span>Nova thread</span>
+              <span>...</span>
             </div>
             <div className="w-full">
               <div className={styles.container2}>
-                <div className={styles.inputArea}><input type="text"></input></div>
+                <div className={styles.inputArea}><input type="text" id='contentThread'></input></div>
                 <div className={styles.textResp}><span>Qualquer pessoa pode responder</span></div>
                 <div className={styles.buttonShare}>
                   <div className="gap-2 w-max h-9">
@@ -30,8 +31,7 @@ export function NewPost() {
               </div>
             </div>
           </div>
-        </div>
-      </div>
+      </DialogContent>
     </Dialog>
   )
 }
