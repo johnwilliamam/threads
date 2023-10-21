43e9968256d55194da6218d1a1792624a3be87f8
John William de Araujo Mesquita <j.williamd.a.m@outlook.com>
2023-10-21 01:59:51 -0300

    feat: add username field

--- src/app/(home)/post/post.tsx
+++ src/app/(home)/post/post.tsx
@@ -1,11 +1,17 @@
 import { NewPost } from "./new-post";
 import { PostAvatar } from "./post-avatar";
 
-export default function HomePost() {
+type PostAvatarProps = {
+    src?: string;
+    fallbackInitials: string;
+    username: string;
+}
+export default function HomePost({ src, fallbackInitials, username }: PostAvatarProps) {
+    
     return (
         <div className="flex justify-between items-center gap-3">
-            <PostAvatar src="https://github.com/johnwilliamam.png" fallbackInitials="JW" />
-            <NewPost />
+            <PostAvatar src={src} fallbackInitials={fallbackInitials}/>
+            <NewPost src={src} fallbackInitials={fallbackInitials} username={username}/>
             <div className="gap-2 w-max h-9">
                 <div className="rounded-xl aria-disabled text-neutral border border-gray-500 hover:cursor-not-allowed">
                     <div className="font-semibold text-sm m-1 text-neutral-500">Publicar</div>
